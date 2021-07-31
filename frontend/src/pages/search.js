e31774fd5fee73788ad54c219deca8e7abcee762
Nicholas Yao <72791134+PShuckle@users.noreply.github.com>
2021-07-31 20:46:59 +1200

    work on search page

--- frontend/src/pages/search.js
+++ frontend/src/pages/search.js
@@ -2,15 +2,27 @@ import React from 'react';
 import '../styling/index.css';
 import '../styling/App.css';
 
-
 function Search() {
 
     return (
       <div>
-        <div className={'noPage'}>
-            <h1>Search results....</h1>
-
-            
+        <div className={'searchPage'}>
+          <img className={"search_page_logo"} src="./noodle.png" />
+          <input
+          className={"search_page_input"}
+          type="search"
+          placeholder="Search"
+          id="site-search"
+          name="q"
+          aria-label="Search through data"
+          ></input>
+          <button className={"search_page_button"}>I'm Feeling Lazy</button>
+          <div class="search_page_term">
+            Showing results for: 
+          </div> 
+          <div class="results">
+            a
+          </div> 
         </div>
        
       </div>
