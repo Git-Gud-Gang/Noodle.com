e0087a8c06780bf401a3d05d3982779f0c6204d3
Bac0nEater <69305722+Bac0nEater@users.noreply.github.com>
2021-08-01 12:23:11 +1200

    list

--- frontend/src/pages/search.js
+++ frontend/src/pages/search.js
@@ -1,33 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styling/index.css';
 import '../styling/App.css';
+import ContentList from '../components/ContentList';
+
+const initialContents = [
+  {
+    id: "cat",
+    content: "Make the workshop last minute",
+    type: "joke"
+  },
+  {
+    id: "cat",
+    content: "Make the workshop last minute",
+    type: "fact"
+  },
+  {
+    id: "dog",
+    content: "Make the workshop last minute",
+    type: "video"
+  }
+];
 
 function Search() {
+  const [cont, setContents] = useState(initialContents)
 
-    return (
-      <div>
-        <div className={'searchPage'}>
-          <img className={"search_page_logo"} src="./noodle.png" />
-          <input
+  function handleClick(index) {
+    const newContents = [...cont];
+    newContents[index].isComplete = !cont[index].isComplete;
+    // newContents[index].description = "Hello World!!"
+    setContents(newContents);
+  }
+
+
+  return (
+    <div>
+      <div className={'searchPage'}>
+        <img className={"search_page_logo"} src="./noodle.png" />
+        <input
           className={"search_page_input"}
           type="search"
           placeholder="Search"
           id="site-search"
           name="q"
           aria-label="Search through data"
-          ></input>
-          <button className={"search_page_button"}>I'm Feeling Lazy</button>
-          <div class="search_page_term">
-            Showing results for: 
-          </div> 
-          <div class="results">
-            a
-          </div> 
+        ></input>
+        <button className={"search_page_button"}>I'm Feeling Lazy</button>
+        <div class="search_page_term">
+          Showing results for:
         </div>
-       
+        <div class="results">
+
+          <nav>
+            <ul>
+              <li class="nav-item"><a href="#">Home</a></li>
+              <li class="nav-item"><a href="#">Products</a></li>
+              <li class="nav-item"><a href="#">Store</a></li>
+              <li class="nav-item"><a href="#">Team</a></li>
+            </ul>
+          </nav>
+
+
+          <ContentList cont={cont} handleClick={handleClick} />
+
+
+        </div>
       </div>
-  
-    );
-  }
-  
-  export default Search;
+
+    </div>
+
+  );
+}
+
+export default Search;
